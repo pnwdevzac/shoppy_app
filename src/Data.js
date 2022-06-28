6103c4a7b55bea39bea5121574b0c9e8df807d91
Zachary Hartman <zbh206@gmail.com>
2022-06-28 13:54:28 -0700

    adventuretravel master

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Utah",
+    price: 450,
+    desc: "Over the course of five days, we will teach you the technical skills to confidently climb in relative safety.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Ecuador",
+    price: 1200,
+    desc: "Two weeks skills training, including crevasse rescue and a summit of Cotopaxi",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Patagonia",
+    price: 1800,
+    desc: "Twenty day trek through the high country in Patagonia.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mt. Rainier, Washington.",
+    price: 600,
+    desc: "Learn technical Alpine mountaineering skills, and summit Mt. Rainier.",
+    img: "images/img-4.jpg",
+  },
+];
